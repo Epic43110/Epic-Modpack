c521133b7216020d6359598d3a1e1b80c19aa5f9
Epic43110 <82621127+Epic43110@users.noreply.github.com>
2022-12-01 12:52:42 -0600

    Update Epic Modpack.js

--- Epic Modpack.js
+++ Epic Modpack.js
@@ -1,3 +1,22 @@
 alert("Epic Modpack loaded!")
 
-const version = 0.4;
+function fetchVersion() {
+  fetch("https://raw.githubusercontent.com/Epic43110/Epic-Modpack/main/version.txt", {mode: "cors"})
+    .then((response) => response.text())
+    .then((text) => return(text))
+}
+
+var clientVersion = fetchVersion();
+
+if (localStorage.epicVersion != clientVersion) {
+  localStorage.epicVersion = clientVersion
+}
+
+setInterval(function() {
+  var newVersion = fetchVersion();
+  if (clientVersion < newVersion) {
+    if (confirm("New Update for Epic Modpack!\n\nModpack will update next time you open BetterMope.\nWould you like to update right now? (This will refresh your page)")) {
+      localStorage.epicVersion = newVersion;
+      location.reload();
+  }
+}, 5000)
