5dde79d84d6114aaa391f31dae3919ccebfc1ed9
Epic43110 <82621127+Epic43110@users.noreply.github.com>
2022-12-01 22:58:34 -0600

    Update Epic Modpack.js

--- Epic Modpack.js
+++ Epic Modpack.js
@@ -1,23 +1,35 @@
 alert("Epic Modpack loaded!")
 
+var epicModpack = {
+  clientVersion : 0,
+  firstCheck : false,
+  showUpdate : true,
+  updateChecks : 0
+}
+
 function fetchVersion() {
   fetch("https://raw.githubusercontent.com/Epic43110/Epic-Modpack/main/version.txt", {mode: "cors"})
     .then((response) => response.text())
-    .then((text) => { return(text) })
+    .then((text) => {
+      epicModpack.updateChecks += 1;
+      text = Number(text.replaceAll('\n', ''));
+      if (epicModpack.firstCheck) {
+        epicModpack.clientVersion = text;
+        if (localStorage.epicVersion) {
+          localStorage.epicVersion = epicModpack.clientVersion;
+        }
+        epicModpack.firstCheck = false;
+        return;
+      }
+
+      if (epicModpack.clientVersion < text && epicModpack.showUpdate) {
+        epicModpack.showUpdate = false;
+        if (confirm("Update required for Epic Modpack!\n\nModpack will update next time you open BetterMope.\nWould you like to update right now? (This will refresh your page)")) {
+          localStorage.epicVersion = text;
+          location.reload();
+        }
+      }
+    })
 }
 
-var clientVersion = fetchVersion();
-
-if (localStorage.epicVersion != clientVersion) {
-  localStorage.epicVersion = clientVersion
-}
-
-setInterval(function() {
-  var newVersion = fetchVersion();
-  if (clientVersion < newVersion) {
-    if (confirm("New Update for Epic Modpack!\n\nModpack will update next time you open BetterMope.\nWould you like to update right now? (This will refresh your page)")) {
-      localStorage.epicVersion = newVersion;
-      location.reload();
-    }
-  }
-}, 5000)
+setInterval(fetchVersion, 5000)
