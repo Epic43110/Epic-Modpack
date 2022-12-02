2da45dd6bdccee5bb8d002433d956a08272d20b2
Epic43110 <82621127+Epic43110@users.noreply.github.com>
2022-12-01 23:12:12 -0600

    Update Epic Modpack.js

--- Epic Modpack.js
+++ Epic Modpack.js
@@ -1,36 +1,37 @@
 alert("Epic Modpack loaded!")
 
 var epicModpack = {
-  clientVersion : 0,
-  firstCheck : false,
-  showUpdate : true,
-  updateChecks : 0
+    clientVersion: 0,
+    firstCheck: false,
+    showUpdate: true,
+    updateChecks: 0
 }
 window.epicModpack = epicModpack;
 
 function fetchVersion() {
-  fetch("https://raw.githubusercontent.com/Epic43110/Epic-Modpack/main/version.txt", {mode: "cors"})
-    .then((response) => response.text())
-    .then((text) => {
-      epicModpack.updateChecks += 1;
-      text = Number(text.replaceAll('\n', ''));
-      if (epicModpack.firstCheck) {
-        epicModpack.clientVersion = text;
-        if (localStorage.epicVersion) {
-          localStorage.epicVersion = epicModpack.clientVersion;
-        }
-        epicModpack.firstCheck = false;
-        return;
-      }
+    fetch("https://raw.githubusercontent.com/Epic43110/Epic-Modpack/main/version.txt", {
+            mode: "cors"
+        })
+        .then((response) => response.text())
+        .then((text) => {
+            epicModpack.updateChecks += 1;
+            text = Number(text.replaceAll('\n', ''));
+            if (epicModpack.firstCheck) {
+                epicModpack.clientVersion = text;
+                if (localStorage.epicVersion) {
+                    localStorage.epicVersion = epicModpack.clientVersion;
+                }
+                epicModpack.firstCheck = false;
+            }
 
-      if (epicModpack.clientVersion < text && epicModpack.showUpdate) {
-        epicModpack.showUpdate = false;
-        if (confirm("Update required for Epic Modpack!\n\nModpack will update next time you open BetterMope.\nWould you like to update right now? (This will refresh your page)")) {
-          localStorage.epicVersion = text;
-          location.reload();
-        }
-      }
-    })
+            if (epicModpack.clientVersion < text && epicModpack.showUpdate) {
+                epicModpack.showUpdate = false;
+                if (confirm("Update required for Epic Modpack!\n\nModpack will update next time you open BetterMope.\nWould you like to update right now? (This will refresh your page)")) {
+                    localStorage.epicVersion = text;
+                    location.reload();
+                }
+            }
+        })
 }
 
 setInterval(fetchVersion, 5000)
